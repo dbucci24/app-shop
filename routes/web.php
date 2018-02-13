4829ad0d350d599b8e72f9a43754cc89a1d95a56
David Bucci <davidbucci24@outlook.es>
2018-02-13 07:24:12 -0300

    Base project

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+Route::get('/', 'TestController@welcome'); 
+
+
+/*Route::get('/prueba', function() {
+
+	return "Hola, soy la ruta de prueba";
+})*/
+;
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
